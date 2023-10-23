31504a3721a115424739f53886c30c1698472afa
Santiago Peraza <perazasantiago93@gmail.com>
2023-10-23 14:04:54 -0300

    Adding solutions until error handling

--- exercises/13_error_handling/errors2.rs
+++ exercises/13_error_handling/errors2.rs
@@ -3,7 +3,7 @@
 // Say we're writing a game where you can buy items with tokens. All items cost
 // 5 tokens, and whenever you purchase items there is a processing fee of 1
 // token. A player of the game will type in how many items they want to buy, and
-// the `total_cost` function will calculate the total cost of the items. Since
+// the `total_cost` function will calculate the total cost of the tokens. Since
 // the player typed in the quantity, though, we get it as a string-- and they
 // might have typed anything, not just numbers!
 //
@@ -19,14 +19,13 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
 }
