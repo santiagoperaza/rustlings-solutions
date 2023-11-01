5c7e979bc47fe9e8a37f9dbc36c1ddd57d971557
Santiago Peraza <perazasantiago93@gmail.com>
2023-11-01 14:24:40 -0300

    Solutions for lifetimes, tests and iterators

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -10,12 +10,13 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        let x = 10;
+        let y = 5;
+        assert!(x + y == 15, "Expected x + y to equal 15");
     }
 }
--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -6,12 +6,13 @@
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        let x = 15;
+        let y = 30 - x;
+        assert_eq!(x, y, "Expected x to equal y");
     }
 }
